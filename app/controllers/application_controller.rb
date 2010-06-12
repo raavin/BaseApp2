aa450cdeb1d80bd736d75f7757d0e2626160f062
Danny McClelland <mail@dannyweb.net>
2010-06-12 15:03:26 +0100

    Added mobile version and structure for adding mobile versions of future models.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,8 @@ class ApplicationController < ActionController::Base
   include AuthenticatedSystem
   include RoleRequirementSystem
   
+  before_filter :prepare_for_mobile
+  
   helper :all # include all helpers, all the time
   filter_parameter_logging :password, :password_confirmation
   
@@ -16,4 +18,21 @@ class ApplicationController < ActionController::Base
   # See ActionController::RequestForgeryProtection for details
   # Uncomment the :secret if you're not using the cookie session store
   protect_from_forgery # :secret => '9fe6825f97cc334d88925fde5c4808a8'
+
+  private
+
+  def mobile_device?
+    if session[:mobile_param]
+      session[:mobile_param] == "1"
+    else
+      request.user_agent =~ /Mobile|webOS/
+    end
+  end
+  helper_method :mobile_device?
+
+  def prepare_for_mobile
+    session[:mobile_param] = params[:mobile] if params[:mobile]
+    request.format = :mobile if mobile_device?
+  end
+
 end
